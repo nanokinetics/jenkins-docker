c0b99d427fbabba770a4b5eae1cedba2d460667e
phil.madden <philmadden83@gmail.com>
2016-07-29 10:59:30 -0500

    Adding build trigger option

--- src/main/java/com/vivid/docker/PushImagePostBuildStep.java
+++ src/main/java/com/vivid/docker/PushImagePostBuildStep.java
@@ -12,40 +12,58 @@ public class PushImagePostBuildStep extends DockerPostBuildStep {
 
     private final String image;
     private final String tag;
+    private final String buildTrigger;
     private final boolean disableContentTrust;
     private final boolean fail;
 
     @DataBoundConstructor
     public PushImagePostBuildStep(String image,
                                   String tag,
+                                  String buildTrigger,
                                   boolean disableContentTrust,
                                   boolean fail) {
         this.image = image;
         this.tag = tag;
         this.disableContentTrust = disableContentTrust;
         this.fail = fail;
+        this.buildTrigger = buildTrigger;
     }
 
     @Override
     public boolean perform(AbstractBuild build, Launcher launcher, BuildListener listener) {
-        try {
-            EnvVars environment = getEnvironment(build, listener);
+        if (shouldTriggerBuild(build)) {
+            try {
+                EnvVars environment = getEnvironment(build, listener);
 
-            String tagName = FieldUtil.getMacroReplacedFieldValue(tag, environment);
+                String tagName = FieldUtil.getMacroReplacedFieldValue(tag, environment);
 
-            PushCommandArgumentBuilder pushCommandArgumentBuilder = new PushCommandArgumentBuilder()
-                    .disableContentTrust(disableContentTrust)
-                    .image(String.format("%s:%s", image, tagName));
+                PushCommandArgumentBuilder pushCommandArgumentBuilder = new PushCommandArgumentBuilder()
+                        .disableContentTrust(disableContentTrust)
+                        .image(String.format("%s:%s", image, tagName));
 
-            listener.getLogger().append(String.format("Pushing \"%s:%s\" to Docker Hub.", image, tagName));
+                listener.getLogger().append(String.format("Pushing \"%s:%s\" to Docker Hub.", image, tagName));
 
-            DockerCommandExecutor command = new DockerCommandExecutor(pushCommandArgumentBuilder, environment);
-            return command.execute(build, launcher, listener);
+                DockerCommandExecutor command = new DockerCommandExecutor(pushCommandArgumentBuilder, environment);
+                return command.execute(build, launcher, listener);
 
-        } catch (EnvironmentConfigurationException e) {
-            listener.getLogger().append(e.getMessage());
-            return false;
+            } catch (EnvironmentConfigurationException e) {
+                listener.getLogger().append(e.getMessage());
+                return false;
+            }
         }
+        return true;
+    }
+
+    private boolean shouldTriggerBuild(AbstractBuild build) {
+        switch (buildTrigger) {
+            case "SUCCESS":
+                if (build.getResult() == Result.SUCCESS || build.getResult() == Result.UNSTABLE) {
+                    return true;
+                }
+            case "FAILURE":
+                return build.getResult() == Result.FAILURE;
+        }
+        return false;
     }
 
     public String getImage() {
@@ -58,6 +76,14 @@ public class PushImagePostBuildStep extends DockerPostBuildStep {
 
     public boolean isFail() {
         return fail;
+    }
+
+    public boolean isDisableContentTrust() {
+        return disableContentTrust;
+    }
+
+    public String getBuildTrigger() {
+        return buildTrigger;
     }
 }
 
