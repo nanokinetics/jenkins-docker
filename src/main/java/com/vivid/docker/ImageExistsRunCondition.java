6d907198cfac79df1aa55b060c959651086e9b0a
phil.madden <philmadden83@gmail.com>
2016-08-31 14:13:57 -0500

    run-cond

---
+++ src/main/java/com/vivid/docker/ImageExistsRunCondition.java
@@ -0,0 +1,65 @@
+package com.vivid.docker;
+
+import com.vivid.hudson.docker.*;
+import com.vivid.hudson.docker.util.*;
+import hudson.*;
+import hudson.model.*;
+import jenkins.model.*;
+import org.jenkins_ci.plugins.run_condition.*;
+import org.kohsuke.stapler.*;
+
+import java.io.*;
+
+
+public class ImageExistsRunCondition extends RunCondition {
+    protected static final int SUCCESS = 0;
+    private final String tag;
+    private final String variant;
+
+    @DataBoundConstructor
+    public ImageExistsRunCondition(String tag,
+                                   String variant) {
+        this.tag = tag;
+        this.variant = variant;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public String getVariant() {
+        return variant;
+    }
+
+    @Override
+    public boolean runPrebuild(AbstractBuild<?, ?> abstractBuild, BuildListener buildListener) throws Exception {
+        return true;
+    }
+
+    @Override
+    public boolean runPerform(AbstractBuild<?, ?> abstractBuild, BuildListener buildListener) throws Exception {
+        EnvVars envVars = abstractBuild.getEnvironment(buildListener);
+        String imageTag = FieldUtil.getMacroReplacedFieldValue(tag, envVars).toLowerCase();
+        String imageVariant = FieldUtil.getMacroReplacedFieldValue(variant, envVars).toLowerCase();
+
+        Launcher launcher = Jenkins.getInstance().createLauncher(buildListener);
+
+        try {
+            int result = launcher.launch()
+                    .cmdAsSingleString(BuildHelper.getDockerBinary() + " history " + imageTag + ":" + imageVariant)
+                    .quiet(true)
+                    .envs(envVars)
+                    .join();
+
+            return result == SUCCESS;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+
+}
